bf0960f7aab684f23e6058f715f918f9ca78dd50
cmumme <camrenmumme@gmail.com>
2024-12-24 12:56:00 -0500

    Remove component handling in Game

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,5 @@
 import { Loader } from "@rbxts/loader"
-import { Loop, AnySystem, World, AnyComponent } from "@rbxts/matter"
+import { Loop, AnySystem, World } from "@rbxts/matter"
 import Object from "@rbxts/object-utils"
 import { RunService } from "@rbxts/services"
 
@@ -21,7 +21,6 @@ const CURRENT_RUNTIME_LOOP_EVENTS = RunService.IsServer() ? {
 	stepped: RunService.Stepped
 }
 
-export type ComponentList = Record<string, (...args: any[]) => AnyComponent>
 export type GamePluginCtor<T extends GamePlugin = GamePlugin> = new (game: Game<never>) => T
 
 /**
@@ -75,57 +74,24 @@ export abstract class GamePlugin {
  * import { ServerStorage } from "@rbxts/services"
  * import { Game } from "@rbxts/matter-bootstrap"
  * import { Replication } from "@rbxts/matter-bootstrap/plugins"
- * import Components from "shared/components"
  * 
  * new Game<GamePlugins>({
  * 	systemsFolder: ServerStorage.systems,
- * 	componentList: Components,
  * 	plugins: {
  * 		replication: Replication({ }),
  * 	},
  * }).begin()
  * ```
- * 
- * ```ts
- * // src/systems/greetPlayers.ts
- * import Components from "shared/components"
- * 
- * export = function(exampleGame: ExampleGame) {
- * 	for(const [,playerRecord] in exampleGame.world.queryChanged(Components.Player)) {
- * 		if(playerRecord.new && !playerRecord.old) {
- * 			// player has joined!
- * 			exampleGame.plugins.networking.send(playerRecord.new.instance, "hello")
- * 		}
- * 	}
- * }
- * ```
- * 
- * ```ts
- * // src/shared/components.ts
- * export = {
- * 	Player: new component("Player")
- * }
- * ```
- * 
- * ```ts
- * // src/game.d.ts
- * type ExampleGame = Game<{
- * 	networking: Network,
- * 	playerEntities: PlayerEntities
- * }>
- * ```
  */
 export class Game<T extends { [name: string]: GamePlugin }> {
 	public readonly world = new World()
 	public readonly plugins: T = { } as T
-	public readonly components: ComponentList
 	public readonly loop: Loop<[ Game<T> ]>
 	private readonly systems: AnySystem[]
 
 	public constructor(
 		private readonly config: GameConfig<T>
 	) {
-		this.components = config.componentList ?? { }
 		this.systems = Loader.Load(config.systemsFolder)
 		this.loop = new Loop(this)
 
