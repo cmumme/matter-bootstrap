b4283d58edb3749c18975990a41f2667c90f0147
cmumme <camrenmumme@gmail.com>
2025-01-04 18:36:59 -0500

    Revert "Begin a port of Matter's example game"
    This reverts commit 37bb4a9c53cb99b7a7e7a1bb127b54cf3cfec119.

--- examples/attack-of-the-killer-roombas/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-import bootstrap from "shared/bootstrap"
-
-declare global {
-	// A globally-scoped game type allows systems to properly type
-	// their arguments and access plugins with ease
-	type AttackOfTheKillerRoombas = ReturnType<typeof bootstrap>
-}
